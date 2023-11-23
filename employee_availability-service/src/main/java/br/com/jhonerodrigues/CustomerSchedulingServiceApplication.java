208b2ef672504ade8b9e77340e69f5251945151a
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-11-22 22:32:31 -0300

    Created project

---
+++ employee_availability-service/src/main/java/br/com/jhonerodrigues/CustomerSchedulingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonerodrigues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSchedulingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSchedulingServiceApplication.class, args);
+	}
+
+}
