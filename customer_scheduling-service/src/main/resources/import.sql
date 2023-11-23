4c845715212c1aa0771b38b4dae7e23ee91dcfe9
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-11-23 15:28:37 -0300

    Custumer - Job Data added to the database

---
+++ customer_scheduling-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO job (duration_minutes, price, name) VALUES (60,30.00,'Corte de cabelo');
+INSERT INTO job (duration_minutes, price, name) VALUES (30,10.00,'Sobrancelha');
+INSERT INTO job (duration_minutes, price, name) VALUES (60,20.00,'Barba');
