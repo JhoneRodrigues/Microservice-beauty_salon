a2ade2d7e194f08145324458e7ce8a9bd40568fd
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-12-05 12:11:32 -0300

    Feat: Custumer - Add Exception handling

--- customer_scheduling-service/src/main/java/br/com/jhonerodrigues/core/domain/enums/StandardTimes.java
+++ customer_scheduling-service/src/main/java/br/com/jhonerodrigues/core/domain/enums/StandardTimes.java
@@ -2,7 +2,7 @@ package br.com.jhonerodrigues.core.domain.enums;
 
 public enum StandardTimes {
 
-	T09_11(1), T11_13(2), T14_16(3), T16_18(4), T18_20(5);
+	T09_11(0), T11_13(1), T14_16(2), T16_18(3), T18_20(4);
 
 	private int code;
 
