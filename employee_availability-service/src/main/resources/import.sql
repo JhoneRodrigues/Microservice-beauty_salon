b023e44288cff3eeb598ebbab851554f3f455940
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-12-10 13:04:49 -0300

    Docs: Employee - add data to db

---
+++ employee_availability-service/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO professional (description, name, url_image) VALUES ('O melhor do mês', 'João', 'https://testandoDB');
+INSERT INTO professional (description, name, url_image) VALUES ('Experiência de 6 anos', 'Pedro', 'https://blablabla');
+
+INSERT INTO scheduling (col_day, col_time, professional_id, client_phone) VALUES ('2023-12-07', 1, 1, '(11) 92222-3030');
+INSERT INTO scheduling (col_day, col_time, professional_id, client_phone) VALUES ('2023-12-28', 3, 2, '(11) 93332-3230');
